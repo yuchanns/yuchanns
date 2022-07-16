ffbfe4f548b6ed4b543311f017d9267212599abc
yuchanns <me@yuchanns.xyz>
2022-07-16 16:25:28 +0800

    improve: make tasks options optional

--- packages/@vpzk/plugin-tasks/src/node/tasksPlugin.ts
+++ packages/@vpzk/plugin-tasks/src/node/tasksPlugin.ts
@@ -2,10 +2,10 @@ import type { Plugin, PluginObject } from '@vuepress/core'
 import tasks from '@hedgedoc/markdown-it-task-lists'
 
 export interface tasksOptions {
-    enabled: boolean
-    lineNumber: boolean
-    label: boolean
-    labelAfter: boolean
+    enabled?: boolean
+    lineNumber?: boolean
+    label?: boolean
+    labelAfter?: boolean
 }
 
 export const tasksPlugin = (opts: tasksOptions): Plugin => {

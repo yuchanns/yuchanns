79e1204130550b7b92e9545b0c5567e4973a69a6
yuchanns <me@yuchanns.xyz>
2022-08-14 03:29:14 +0800

    feat: better-list

---
+++ packages/@vpzk/plugin-better-list/src/node/index.ts
@@ -0,0 +1,5 @@
+import { betterListPlugin } from './betterListPlugin'
+
+export * from './betterListPlugin'
+
+export default betterListPlugin
--- site/.vuepress/config.ts
+++ site/.vuepress/config.ts
@@ -4,6 +4,7 @@ import { localTheme } from './theme'
 import { katexPlugin } from '@renovamen/vuepress-plugin-katex'
 import { tasksPlugin } from '@vpzk/plugin-tasks'
 import { searchPlugin } from '@vuepress/plugin-search'
+import { betterListPlugin } from '@vpzk/plugin-better-list'
 
 const isProd = process.env.NODE_ENV === 'production'
 
@@ -26,6 +27,7 @@ export default defineUserConfig({
     katexPlugin(),
     isProd ? shikiPlugin({ theme: 'github-dark' }) : [],
     tasksPlugin({ enabled: true }),
-    searchPlugin({ getExtraFields: (page) => page.content.split('\n\n') })
+    searchPlugin({ getExtraFields: (page) => page.content.split('\n\n') }),
+    betterListPlugin()
   ]
 })

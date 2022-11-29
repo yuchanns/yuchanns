e401bb991412e8fd79321639ccf2500c6e35935c
Hanchin Hsieh <me@yuchanns.xyz>
2022-11-30 07:04:54 +0800

    Fix global component import bug

--- site/.vitepress/theme/index.ts
+++ site/.vitepress/theme/index.ts
@@ -1,17 +1,18 @@
 import { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import { Layout } from './layout'
-import Tweet from 'vue-tweet'
 
 import './styles/vars.scss'
 import './plugins/better-list/index.scss'
 import './plugins/zettelkasten/index.scss'
+import { defineAsyncComponent } from 'vue'
+
 
 const theme: Theme = {
   ...DefaultTheme,
   enhanceApp: (ctx) => {
     DefaultTheme.enhanceApp(ctx)
-    ctx.app.component('Tweet', Tweet)
+    ctx.app.component('Tweet', defineAsyncComponent(() => import('vue-tweet')))
   },
   Layout: Layout
 }

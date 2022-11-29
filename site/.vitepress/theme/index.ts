78496aedebb1e9d719fdeb4473a95f2efe4d4526
Hanchin Hsieh <me@yuchanns.xyz>
2022-11-30 06:46:19 +0800

    Add post about Asahi Linux

--- site/.vitepress/theme/index.ts
+++ site/.vitepress/theme/index.ts
@@ -1,6 +1,7 @@
 import { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import { Layout } from './layout'
+import Tweet from 'vue-tweet'
 
 import './styles/vars.scss'
 import './plugins/better-list/index.scss'
@@ -8,6 +9,10 @@ import './plugins/zettelkasten/index.scss'
 
 const theme: Theme = {
   ...DefaultTheme,
+  enhanceApp: (ctx) => {
+    DefaultTheme.enhanceApp(ctx)
+    ctx.app.component('Tweet', Tweet)
+  },
   Layout: Layout
 }
 

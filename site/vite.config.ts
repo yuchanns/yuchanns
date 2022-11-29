2d28827d7e39ccd2ef3e5a5aa7e20494f17e97a8
Hanchin Hsieh <me@yuchanns.xyz>
2022-11-29 18:22:25 +0800

    Add analyzer

--- site/vite.config.ts
+++ site/vite.config.ts
@@ -1,5 +1,6 @@
 import { SearchPlugin } from "vitepress-plugin-search"
 import { defineConfig } from "vite"
+import visualizer from "rollup-plugin-visualizer"
 import Segment from 'segment'
 
 // issue: https://github.com/emersonbottero/vitepress-plugin-search/issues/11#issuecomment-1328150584
@@ -15,6 +16,11 @@ const options = {
 
 export default defineConfig({
   plugins: [
-    SearchPlugin(options)
+    SearchPlugin(options),
+    visualizer({
+      open: true,
+      gzipSize: true,
+      brotliSize: true,
+    })
   ],
 });
